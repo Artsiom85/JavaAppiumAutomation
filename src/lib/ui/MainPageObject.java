be02b2df2b68839d6ad342ff919240db712afc23
Artsiom85 <artsiom.karaliou85@gmail.com>
2018-06-14 15:50:07 -0600

    Added refactoring

--- src/lib/ui/MainPageObject.java
+++ src/lib/ui/MainPageObject.java
@@ -1,4 +1,101 @@
 package lib.ui;
 
+<<<<<<< HEAD
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
 public class MainPageObject {
+
+  protected AppiumDriver driver;
+
+  public MainPageObject(AppiumDriver driver)
+
+
+  {
+    this.driver = driver;
+  }
+  public String waitForElementAndGetAttribute(By by, String attribute, String error_message, long timeoutInSeconds) {
+    WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+    return element.getAttribute(attribute);
+  }
+
+  public WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
+    WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+    wait.withMessage(error_message + "\n");
+    return wait.until(
+            ExpectedConditions.presenceOfElementLocated(by)
+    );
+  }
+
+  public WebElement waitForElementPresent(By by, String error_message) {
+    return waitForElementPresent(by, error_message, 5);
+  }
+
+  public WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
+    WebElement element = waitForElementPresent(by, error_message, 5);
+    element.click();
+    return element;
+  }
+
+  public WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds) {
+    WebElement element = waitForElementPresent(by, error_message, 5);
+    element.sendKeys(value);
+    return element;
+  }
+
+  public boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds) {
+    WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+    wait.withMessage(error_message + "\n");
+    return wait.until(
+            ExpectedConditions.invisibilityOfElementLocated(by)
+    );
+  }
+
+  public WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds) {
+    WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+    element.clear();
+    return element;
+  }
+
+  public void swipeElementToLeft(By by, String error_message) {
+    WebElement element = waitForElementPresent(by,
+            error_message,
+            10);
+
+    int left_x = element.getLocation().getX();
+    int right_x = left_x + element.getSize().getWidth();
+    int upper_y = element.getLocation().getY();
+    int lower_y = upper_y + element.getSize().getHeight();
+    int middle_y = (upper_y + lower_y) / 2;
+
+    TouchAction action = new TouchAction(driver);
+    action
+            .press(right_x, middle_y)
+            .waitAction(300)
+            .moveTo(left_x, middle_y)
+            .release()
+            .perform();
+
+  }
+
+  public int getAmountOfElement(By by) {
+    List elements = driver.findElements(by);
+    return elements.size();
+  }
+
+  public void assertElementPresent(By by) {
+    Assert.assertTrue(
+            "Can't find article's title!",
+            driver.findElements(by).size() == 1);
+  }
+=======
+public class MainPageObject {
+>>>>>>> 9ffb5c085682fef9866d0996cdb31437b227ddf6
 }
